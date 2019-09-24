fb29d7fcd2d0550bc80597468b852cfa556bcebb
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-09-24 18:59:56 +0700

    Initial commit

---
+++ src/com/pbo/PBO11K10118902Latihan9Teks.java
@@ -0,0 +1,22 @@
+package com.pbo;
+
+/**
+ *
+ * # Author
+ * Nama : Agung Sepruloh
+ * Kelas : PBO11K
+ * NIM : 10118902
+ * Deskripsi Program : Program ini berisi macam-macam tipe data penampung char/karakter/teks
+ *
+ */
+
+public class PBO11K10118902Latihan9Teks {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newLine = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newLine + "Char: " + tab + "berisi " + c );
+    }
+}
